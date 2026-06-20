e4ed8151f7cc9fa035a11cd2bd44c73e49124f69
connortodd21 <connortodd21@gmail.com>
2026-06-20 15:41:46 -0700

    Add two new game stats

--- scripts/game/stats/player_game_stats.gd
+++ scripts/game/stats/player_game_stats.gd
@@ -16,6 +16,18 @@ func register_players(players: Array[Player]) -> void:
 ### STATS
 #############################################
 var knights := KnightPlayerStats.new()
+var robbers := RobberPlayerStats.new()
+var stolen := ResourcesStolenPlayerStats.new()
+var lost_from_robbing := ResourcesLostFromRobbingPlayerStats.new()
+
+var _columns: Dictionary[String, Callable] = {}
+
+
+func _init() -> void:
+	_columns["Knights"] = knights.get_count
+	_columns["Robbers Placed"] = robbers.get_count
+	_columns["Resources Stolen"] = stolen.get_count
+	_columns["Resources Lost from Robbing"] = lost_from_robbing.get_count
 
 
 func get_players() -> Array[Player]:
@@ -23,11 +35,14 @@ func get_players() -> Array[Player]:
 
 
 func get_headers() -> Array[String]:
-	return ["Knights"]
+	return _columns.keys()
 
 
 func get_values(player: Player) -> Array[int]:
-	return [knights.get_count(player)]
+	var values: Array[int] = []
+	for col in _columns.values():
+		values.append(col.call(player))
+	return values
 
 
 #############################################
@@ -42,6 +57,60 @@ class KnightPlayerStats:
 	func _on_development_card_played(player: Player, card_type: CardTypes.Type) -> void:
 		if card_type == CardTypes.Type.KNIGHT:
 			_counts[player] = _counts.get(player, 0) + 1
+
+	func get_count(player: Player) -> int:
+		return _counts.get(player, 0)
+
+
+#############################################
+### ROBBERS
+#############################################
+class RobberPlayerStats:
+	var _counts: Dictionary[Player, int] = {}
+
+	func _init() -> void:
+		GameSignals.development_card_played.connect(_on_development_card_played)
+		GameSignals.player_rolled.connect(_on_player_rolled)
+
+	func _on_development_card_played(player: Player, card_type: CardTypes.Type) -> void:
+		if card_type == CardTypes.Type.KNIGHT:
+			_counts[player] = _counts.get(player, 0) + 1
+
+	func _on_player_rolled(player: Player, total: int) -> void:
+		if total == 7:
+			_counts[player] = _counts.get(player, 0) + 1
+
+	func get_count(player: Player) -> int:
+		return _counts.get(player, 0)
+
+
+#############################################
+### RESOURCES STOLEN
+#############################################
+class ResourcesStolenPlayerStats:
+	var _counts: Dictionary[Player, int] = {}
+
+	func _init() -> void:
+		GameSignals.resource_stolen.connect(_on_resource_stolen)
+
+	func _on_resource_stolen(thief: Player, _victim: Player, _resource: ResourceTypes.Type) -> void:
+		_counts[thief] = _counts.get(thief, 0) + 1
+
+	func get_count(player: Player) -> int:
+		return _counts.get(player, 0)
+
+
+#############################################
+### RESOURCES LOST
+#############################################
+class ResourcesLostFromRobbingPlayerStats:
+	var _counts: Dictionary[Player, int] = {}
+
+	func _init() -> void:
+		GameSignals.resource_stolen.connect(_on_resource_stolen)
+
+	func _on_resource_stolen(_thief: Player, victim: Player, _resource: ResourceTypes.Type) -> void:
+		_counts[victim] = _counts.get(victim, 0) + 1
 
 	func get_count(player: Player) -> int:
 		return _counts.get(player, 0)
